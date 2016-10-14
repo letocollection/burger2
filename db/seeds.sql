077e2c7afcb3c6783f3cb3575b53315976a672cc
letocollection <leonard@letocollection.com>
2016-10-13 22:02:17 -0700

    launching heroku

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES('big mac', FALSE, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES('chili cheese burger', FALSE, NOW());
+INSERT INTO burgers (burger_name, devoured, date) VALUES('western bacon cheeseburger', FALSE, NOW());
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// static directory
+app.use(express.static(process.cwd() + './public'));
+
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+// var port = 8080;
+
+var port = process.env.PORT || 8080;
+app.listen(port);
